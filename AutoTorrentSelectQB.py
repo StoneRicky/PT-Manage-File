c8e9403d90848124d978a1dbe920a0d15b6b8000
StoneRicky <739799980@qq.com>
2024-03-01 14:26:30 +0800

    交互方式输入内容

--- AutoTorrentSelectQB.py
+++ AutoTorrentSelectQB.py
@@ -5,6 +5,20 @@ import requests
 ##########手动填写区域
 # 基本信息
 
+# 此处应该改为输入
+# 种子信息
+hash = input('请输入哈希值:')
+# 测试文件大小 0-测试(仅统计) 1-修改
+testStatus = input('是否真实修改文件状态？ (0-测试,仅统计 1-修改):')
+# 设置信息 0-不下载 1-正常
+status = 0
+if(testStatus == 1):
+    status = input('请输入设置状态？(0-不下载 1-正常)：')
+# 文件类型
+fileTyle = input('请输入文件类型：')
+# 文件大小
+fileSize = int(input('请输入最大文件大小(MB)：')) * 1024 * 1024
+
 # 读取配置文件
 config = configparser.ConfigParser()
 config.read("general.config", encoding="utf-8")
@@ -18,19 +32,6 @@ port = config.get('QB', 'port'),
 username = config.get('QB', 'username'),
 password = config.get('QB', 'password'),
 )
-
-# 此处应该改为输入
-# 种子信息
-hash = '1e51da5676bf2d537609a54cbb4d6e6099f94a68'
-# 设置信息 0-不下载 1-正常
-status = 1
-# 文件类型
-fileTyle = ''
-# 文件大小
-fileSize = 5100*1024*1024
-# 测试文件大小 0-测试 1-修改
-testStatus = 1
-
 
 ##########执行逻辑
 qbt_client = qbittorrentapi.Client(**conn_info)
